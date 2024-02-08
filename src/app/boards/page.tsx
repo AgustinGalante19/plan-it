add7129f5156c091fce34b40c2e6a19ea89628f9
Agustin Galante <agustin.galante.19@outlook.es>
2024-02-08 20:57:43 -0300

    Basic boards page (in progress)

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 
 function Boards() {
-  return <div>Boards</div>
+  return (
+    <div className='container mx-auto bg-blue-500 p-12'>
+      <h2 className='text-4xl font-bold'>My Boards</h2>
+    </div>
+  )
 }
 
 export default Boards
