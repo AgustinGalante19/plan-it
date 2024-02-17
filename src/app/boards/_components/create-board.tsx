aa8da14c109b220c7f2e75ae55305485b582be2b
Agustin Galante <agustin.galante.19@outlook.es>
2024-02-16 22:35:12 -0300

    Validating board creation errors

--- src/app/boards/_components/create-board.tsx
+++ src/app/boards/_components/create-board.tsx
@@ -13,31 +13,47 @@ import { Plus } from "lucide-react"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import createBoard from "../actions/create-board"
+import { useToast } from "@/components/ui/use-toast"
+import { ButtonTooltip } from "@/components/ui/tooltip"
 
 function CreateBoard() {
   const [isOpen, setIsOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+  const { toast } = useToast()
 
   const handleCreateSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setIsLoading(true)
     const formData = new FormData(e.currentTarget)
-    createBoard(formData).finally(() => {
-      setIsLoading(false)
-      setIsOpen(false)
-    })
+    createBoard(formData)
+      .then((response) => {
+        console.log(response)
+        toast({
+          variant: response.ok ? "success" : "destructive",
+          title: response.message,
+        })
+
+        if (response.ok) {
+          setIsOpen(false)
+        }
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }
 
   return (
     <>
-      <Button
-        variant='rounded'
-        title='Create board'
-        onClick={() => setIsOpen(true)}
-        type='button'
-      >
-        <Plus size={15} />
-      </Button>
+      <ButtonTooltip label='Create board'>
+        <Button
+          variant='rounded'
+          title='Create board'
+          onClick={() => setIsOpen(true)}
+          type='button'
+        >
+          <Plus size={15} />
+        </Button>
+      </ButtonTooltip>
       <Dialog open={isOpen} onOpenChange={() => setIsOpen(false)}>
         <DialogContent className='sm:max-w-[425px]'>
           <DialogHeader>
--- src/app/boards/actions/create-board.ts
+++ src/app/boards/actions/create-board.ts
@@ -7,18 +7,27 @@ import { revalidatePath } from "next/cache"
 export default async function createBoard(formData: FormData) {
   try {
     const user = await currentUser()
-    if (!user?.id) return
-    const rawFormData = Object.fromEntries(formData.entries())
+    if (!user?.id) return { message: "You must be logged in", ok: false }
+
+    const rawFormData = {
+      title: formData.get("title") as string,
+      description: formData.get("description") as string,
+    }
+
+    if (rawFormData.title === "") {
+      return { message: "⚠️ Title is required", ok: false }
+    }
     await client.board.create({
       data: {
         userId: user.id,
-        title: rawFormData.title as string,
-        description: (rawFormData.description as string | undefined) ?? "",
+        title: rawFormData.title,
+        description: rawFormData.description ?? "",
       },
     })
 
     revalidatePath("/boards")
+    return { message: "👍 Board created successfully", ok: true }
   } catch (err) {
-    console.log("error on create board ", err)
+    return { message: "⚠️ Unexpected error on board creation", ok: false }
   }
 }
