d8a1f78115b9c7025cf1fbcd389a87bde4a6cbc7
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-06 20:57:29 -0300

    fixing element size

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,33 +1,33 @@
-"use client"
+'use client';
 
-import { Moon, Sun } from "lucide-react"
-import { useEffect, useState } from "react"
-import { motion } from "framer-motion"
+import { Moon, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 function ThemeSwitcher() {
-  const [currentTheme, setCurrentTheme] = useState<"dark" | "light">("light")
+  const [currentTheme, setCurrentTheme] = useState<'dark' | 'light'>('light');
 
   useEffect(() => {
     const configTheme = () => {
       if (
-        globalThis.localStorage.getItem("theme") === "dark" ||
-        (!("theme" in localStorage) &&
-          window.matchMedia("(prefers-color-scheme: dark)").matches)
+        globalThis.localStorage.getItem('theme') === 'dark' ||
+        (!('theme' in localStorage) &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches)
       ) {
-        document.documentElement.classList.add("dark")
-        globalThis.localStorage.setItem("theme", "dark")
-        setCurrentTheme("dark")
+        document.documentElement.classList.add('dark');
+        globalThis.localStorage.setItem('theme', 'dark');
+        setCurrentTheme('dark');
       } else {
-        document.documentElement.classList.remove("dark")
-        globalThis.localStorage.setItem("theme", "light")
-        setCurrentTheme("light")
+        document.documentElement.classList.remove('dark');
+        globalThis.localStorage.setItem('theme', 'light');
+        setCurrentTheme('light');
       }
-    }
-    configTheme()
-  }, [])
+    };
+    configTheme();
+  }, []);
 
   return (
-    <div className='absolute h-screen flex items-end'>
+    <div className='absolute flex items-end bottom-0'>
       <motion.button
         className='p-3 rounded-full flex justify-center items-center w-12 bg-primary m-2'
         type='button'
@@ -35,24 +35,24 @@ function ThemeSwitcher() {
           rotateY: 50,
         }}
         onClick={() => {
-          document.documentElement.classList.toggle("dark")
-          if (currentTheme === "dark") {
-            setCurrentTheme("light")
-            globalThis.localStorage.setItem("theme", "light")
+          document.documentElement.classList.toggle('dark');
+          if (currentTheme === 'dark') {
+            setCurrentTheme('light');
+            globalThis.localStorage.setItem('theme', 'light');
           } else {
-            setCurrentTheme("dark")
-            globalThis.localStorage.setItem("theme", "dark")
+            setCurrentTheme('dark');
+            globalThis.localStorage.setItem('theme', 'dark');
           }
         }}
       >
-        {currentTheme === "dark" ? (
+        {currentTheme === 'dark' ? (
           <Moon color='white' />
         ) : (
           <Sun color='white' />
         )}
       </motion.button>
     </div>
-  )
+  );
 }
 
-export default ThemeSwitcher
+export default ThemeSwitcher;
